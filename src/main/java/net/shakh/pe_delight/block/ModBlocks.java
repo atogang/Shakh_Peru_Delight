7921ada654d46a89fe71c65945115f0e6dbecde4
Shakh <zalord23@gmail.com>
2026-03-06 17:16:54 -0500

    fin vid 5

--- src/main/java/net/shakh/pe_delight/block/ModBlocks.java
+++ src/main/java/net/shakh/pe_delight/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.shakh.pe_delight.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,6 +26,10 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> RAW_PAVO_BLOCK = registerBlock("raw_pavo_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject<Block> PAVO_ORE = registerBlock("pavo_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3,6)));
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/shakh/pe_delight/item/ModCreativeModTabs.java
+++ src/main/java/net/shakh/pe_delight/item/ModCreativeModTabs.java
@@ -27,6 +27,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.PAVO_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_PAVO_BLOCK.get());
 
+                        pOutput.accept(ModBlocks.PAVO_ORE.get());
+
                     })
                     .build());
 
