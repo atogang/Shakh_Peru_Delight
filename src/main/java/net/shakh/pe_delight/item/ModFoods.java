e7839b80f4e7d80e967c077ce9be285e93f0e529
Shakh <zalord23@gmail.com>
2026-03-18 21:24:00 -0500

    add datagen pt. 2

--- src/main/java/net/shakh/pe_delight/item/ModFoods.java
+++ src/main/java/net/shakh/pe_delight/item/ModFoods.java
@@ -11,6 +11,6 @@ public class ModFoods {
     public static final FoodProperties PASSION_FRUIT_CUTS = new FoodProperties.Builder().nutrition(1).fast()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 400), 0.8f).build();
     public static final FoodProperties PASSION_FRUIT_JUICE = new FoodProperties.Builder().nutrition(4).saturationMod(0.2f)
-            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 1600), 1f).alwaysEat().build();
+            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 1400), 1f).alwaysEat().build();
 
 }
